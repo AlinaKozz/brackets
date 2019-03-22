5f5421ddf68c7a198c7892d438e310a64ac0e931
AlinaKozz <alina.kozz58@gmail.com>
2019-03-22 07:26:18 +0300

    add a function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    var open = [], close = [], stack = [];
+
+    for (var i = 0; i < bracketsConfig.length; i++) {
+        open.splice(i, 0, bracketsConfig[i][0]);
+        close.splice(i, 0, bracketsConfig[i][1]);
+    }
+
+    for (var i = 0; i < str.length; i++) {
+        var bracket = str[i];
+        if (open.indexOf(bracket) !== -1) {
+            if (stack[stack.length - 1] !== close.indexOf(bracket)) {
+                stack.push(open.indexOf(bracket));
+                continue
+            }
+        }
+
+        if (close.indexOf(bracket) !== -1) {
+            var pop = stack.pop();
+            if (close.indexOf(bracket) !== pop) {
+                return false
+            }
+        }
+    }
+
+    return stack.length === 0 ? true : false
+};
